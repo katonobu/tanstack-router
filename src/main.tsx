7256effe282dd8bac0c50b508f8be2c24c2028a1
Nobuo Kato <katonobu4649@gmail.com>
2024-03-11 16:03:07 +0000

    hoge10

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 function App() {
   console.log(`VITE_API_KEY:${import.meta.env.VITE_API_KEY}`)
-  console.log(`VITE_GOOGLE_SHEET_API_KEY:${import.meta.VITE_GOOGLE_SHEET_API_KEY}`) 
+  console.log(`VITE_G_API_KEY:${import.meta.env.VITE_G_API_KEY}`) 
   return (
     <>
     <div>Hello world</div>
     <div>{`VITE_API_KEY:${import.meta.env.VITE_API_KEY}`}</div>
-    <div>{`VITE_GOOGLE_SHEET_API_KEY:${import.meta.VITE_GOOGLE_SHEET_API_KEY}`}</div>
+    <div>{`VITE_G_API_KEY:${import.meta.env.VITE_G_API_KEY}`}</div>
     <div>Hello world2</div>
     </>
   )
